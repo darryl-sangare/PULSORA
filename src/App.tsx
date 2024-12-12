edf935035312068f9730baf7d2a76e67715ba452
chriskoo77 <christopher94260@gmail.com>
2024-12-12 11:47:59 +0100

    ajout du header

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,74 @@
 import { useState } from 'react'
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Header from './assets/components/Header';
 import Footer from './assets/components/Footer';
 import Home from './assets/pages/Home';
 import Navbar from './assets/components/Navbar';
 import Footerr from './assets/components/Footerr';
 
+import Articles from './assets/pages/Articles';
+
+
 const App:React.FC = () => {
   return(
     <>
       <section>
-        <Navbar/>
-        <Header/>
-        <Home/>
-        <Footerr/>
+      <Router>
+      <Routes>
+
+        <Route
+          path="/"
+          element={
+            <>
+              <Header title="ACCUEIL" />
+              <div><Home/></div>
+              <Footerr/>
+            </>
+          }
+        />
+        
+        <Route 
+          path="/articles" element={<Articles />} />
+        <Route
+          path="/saison"
+          element={
+            <>
+              <Header title="SAISON" />
+              <div>Détails de la saison</div>
+            </>
+          }
+        />
+        <Route
+          path="/equipe"
+          element={
+            <>
+              <Header title="EQUIPE" />
+              <div>Présentation de l'équipe</div>
+            </>
+          }
+        />
+        <Route
+          path="/contact"
+          element={
+            <>
+              <Header title="CONTACT" />
+              <div>Contactez-nous</div>
+            </>
+          }
+        />
+        <Route
+          path="/compte"
+          element={
+            <>
+              <Header title="COMPTE" />
+              <div>Votre compte</div>
+            </>
+          }
+        />
+      </Routes>
+    </Router>
 
       </section>
 
---
+++ src/assets/pages/Articles.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/Header";
+
+export default function Articles() {
+
+
+  return (
+    <>
+      <Header title="ARTICLES" />
+
+      
+    </>
+  );
+}
