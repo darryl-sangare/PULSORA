bd14a39e0404bf71be1e804d64bb6105743d358f
jpkam <jpkamdem@proton.me>
2024-12-20 15:00:27 +0100

    modif titre + image mais s'affiche pas chez moi, commentaire des api equipes qui fonctionnent

--- src/pages/Equipe.tsx
+++ src/pages/Equipe.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
 type Position = "GK" | "DEF" | "MF" | "FW";
 type Status = "Suspendu" | "Blessé" | "Opérationnel" | "Inconnu";
@@ -25,47 +25,47 @@ export interface PlayerInterface {
 }
 
 export default function Equipe() {
-  const [teamsData, setTeamsData] = useState<TeamInterface[] | null>(null);
-  const [playersData, setPlayersData] = useState<PlayerInterface[] | null>(
-    null
-  );
+  // const [teamsData, setTeamsData] = useState<TeamInterface[] | null>(null);
+  // const [playersData, setPlayersData] = useState<PlayerInterface[] | null>(
+  //   null
+  // );
 
-  useEffect(() => {
-    const fetchTeamsDatas = async () => {
-      try {
-        const res = await fetch("http://localhost:3000/teams");
-        if (!res.ok) {
-          throw new Error(
-            `Erreur dans la récupération des données des matchs : ${res.status}`
-          );
-        }
+  // useEffect(() => {
+  //   const fetchTeamsDatas = async () => {
+  //     try {
+  //       const res = await fetch("http://localhost:3000/teams");
+  //       if (!res.ok) {
+  //         throw new Error(
+  //           `Erreur dans la récupération des données des matchs : ${res.status}`
+  //         );
+  //       }
 
-        const datas = await res.json();
-        setTeamsData(datas.data);
-      } catch (e) {
-        console.log(e);
-      }
-    };
+  //       const datas = await res.json();
+  //       setTeamsData(datas.data);
+  //     } catch (e) {
+  //       console.log(e);
+  //     }
+  //   };
 
-    const fetchPlayersDatas = async () => {
-      try {
-        const res = await fetch("http://localhost:3000/players");
-        if (!res.ok) {
-          throw new Error(
-            `Erreur dans la récupération des données des matchs : ${res.status}`
-          );
-        }
+  //   const fetchPlayersDatas = async () => {
+  //     try {
+  //       const res = await fetch("http://localhost:3000/players");
+  //       if (!res.ok) {
+  //         throw new Error(
+  //           `Erreur dans la récupération des données des matchs : ${res.status}`
+  //         );
+  //       }
 
-        const datas = await res.json();
-        setPlayersData(datas.data);
-      } catch (e) {
-        console.log(e);
-      }
-    };
+  //       const datas = await res.json();
+  //       setPlayersData(datas.data);
+  //     } catch (e) {
+  //       console.log(e);
+  //     }
+  //   };
 
-    fetchTeamsDatas();
-    fetchPlayersDatas();
-  }, []);
+  //   fetchTeamsDatas();
+  //   fetchPlayersDatas();
+  // }, []);
   return (
     <>
       <p>equipe works!</p>
