37ee2cce0061001a2b814fe4ebb045ac7917d37b
jpkam <jpkamdem@proton.me>
2024-12-20 16:40:21 +0100

    correctif affichage de liste des joueurs

--- src/pages/Saison.tsx
+++ src/pages/Saison.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import { PlayerInterface } from "./Equipe";
+import { PlayerInterface, TeamInterface } from "./Equipe";
+// import dateFormat from "dateformat";
 
 export interface GameInterface {
   id: number;
@@ -11,10 +12,8 @@ export interface GameInterface {
 }
 
 export default function Saison() {
-  const [gamesData, setGamesData] = useState<GameInterface[] | null>(null);
-  const [playersData, setPlayersData] = useState<PlayerInterface[] | null>(
-    null
-  );
+  const [gamesData, setGamesData] = useState<GameInterface[]>([]);
+  const [teamsData, setTeamsData] = useState<TeamInterface[]>([]);
 
   useEffect(() => {
     const fetchGamesData = async () => {
@@ -32,9 +31,9 @@ export default function Saison() {
       }
     };
 
-    const fetchPlayersDatas = async () => {
+    const fetchTeamsData = async () => {
       try {
-        const res = await fetch("http://localhost:3000/players");
+        const res = await fetch("http://localhost:3000/teams");
         if (!res.ok) {
           throw new Error(
             `Erreur dans la récupération des données des matchs : ${res.status}`
@@ -42,13 +41,13 @@ export default function Saison() {
         }
 
         const datas = await res.json();
-        setPlayersData(datas.data);
+        setTeamsData(datas.data);
       } catch (e) {
         console.log(e);
       }
     };
 
-    fetchPlayersDatas();
+    fetchTeamsData();
     fetchGamesData();
   }, []);
 
@@ -61,69 +60,101 @@ export default function Saison() {
 
         {gamesData &&
           gamesData.map((game) => (
-            <div>
-              <div key={game.id} className="flex justify-center ">
-                <div className="text-center relative bg-white border-2 border-gray-100 rounded-t-xl  w-3/6 py-3">
-                  <div className="absolute left-10 bg-green-400 px-2  py-0.5 rounded-md">
-                    • Terminé
-                  </div>
-                  <div className="text-gray-400 font-medium ">
-                    Match Amical - Aujourd'hui
-                  </div>
-                </div>
-              </div>
-              <div className="flex justify-center">
-                <div className="flex justify-between w-3/6 border-gray-100 border-l-2 border-r-2 border-b-2 rounded-b-xl drop-shadow-sm ">
-                  <div className="w-1/3 p-6">
-                    <div className="flex items-center justify-center ">
-                      <div className="w-28 h-28 bg-gray-300 rounded-full flex items-center justify-center border-2 border-gray-300">
-                        <div className="w-20 h-20 bg-white rounded-full flex items-center justify-center drop-shadow-lg">
-                          <div className="w-14 h-14 bg-black rounded-full"></div>
-                        </div>
-                      </div>
+            <>
+              <div>
+                <div key={game.id} className="flex justify-center ">
+                  <div className="text-center relative bg-white border-2 border-gray-100 rounded-t-xl  w-3/6 py-3">
+                    <div className="absolute left-10 bg-green-400 px-2  py-0.5 rounded-md">
+                      • Terminé
                     </div>
-                    <div className="text-center  ">
-                      <div className="my-4">{game.homeTeamId}</div>
-                      {playersData &&
-                        playersData.map((player) =>
-                          player.id === game.homeTeamId ? (
-                            <div key={player.id}>
-                              {player.firstname} {player.lastname} -{" "}
-                              {player.position}
-                            </div>
-                          ) : undefined
-                        )}
-                    </div>
-                  </div>
-                  <div className="w-1/3  p-6 flex justify-center  space-x-8 my-10">
-                    <div>{game.homeScore}</div>
-                    <div>:</div>
-                    <div>{game.awayScore}</div>
-                  </div>
-                  <div className="w-1/3 p-6">
-                    <div className="flex items-center justify-center ">
-                      <div className="w-28 h-28 bg-gray-300 rounded-full flex items-center justify-center border-2 border-gray-300">
-                        <div className="w-20 h-20 bg-white rounded-full flex items-center justify-center drop-shadow-lg">
-                          <div className="w-14 h-14 bg-red-600 rounded-full"></div>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="text-center">
-                      <div className="my-4">{game.awayTeamId}</div>
-                      {playersData &&
-                        playersData.map((player) =>
-                          player.id === game.awayTeamId ? (
-                            <div key={player.id}>
-                              {player.firstname} {player.lastname} -{" "}
-                              {player.position}
-                            </div>
-                          ) : undefined
-                        )}
+                    <div className="text-gray-400 font-medium ">
+                      Match Amical -{" "}
+                      {new Intl.DateTimeFormat("fr-FR", {
+                        dateStyle: "long",
+                        timeStyle: "short",
+                      }).format(new Date(game.date))}
                     </div>
                   </div>
                 </div>
+                <div className="flex justify-center">
+                  <div className="flex justify-between w-3/6 border-gray-100 border-l-2 border-r-2 border-b-2 rounded-b-xl drop-shadow-sm ">
+                    <div className="w-1/3 p-6">
+                      <div className="flex items-center justify-center ">
+                        <div className="w-28 h-28 bg-gray-300 rounded-full flex items-center justify-center border-2 border-gray-300">
+                          <div className="w-20 h-20 bg-white rounded-full flex items-center justify-center drop-shadow-lg">
+                            <div className="w-14 h-14 bg-black rounded-full"></div>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="text-center  ">
+                        <div className="my-4">
+                          {teamsData.map((team) => (
+                            <>
+                              {team.id === game.homeTeamId ? (
+                                <p key={team.id}>{team.name}</p>
+                              ) : undefined}
+                            </>
+                          ))}
+                        </div>
+                        {teamsData.map((team) => (
+                          <>
+                            {team.players.map((player: PlayerInterface) => (
+                              <>
+                                {player.teamId === game.homeTeamId ? (
+                                  <p key={player.id}>
+                                    {player.firstname} {player.lastname} -{" "}
+                                    {player.position}
+                                  </p>
+                                ) : undefined}
+                              </>
+                            ))}
+                          </>
+                        ))}
+                      </div>
+                    </div>
+                    <div className="w-1/3  p-6 flex justify-center  space-x-8 my-10">
+                      <div>{game.homeScore}</div>
+                      <div>:</div>
+                      <div>{game.awayScore}</div>
+                    </div>
+                    <div className="w-1/3 p-6">
+                      <div className="flex items-center justify-center ">
+                        <div className="w-28 h-28 bg-gray-300 rounded-full flex items-center justify-center border-2 border-gray-300">
+                          <div className="w-20 h-20 bg-white rounded-full flex items-center justify-center drop-shadow-lg">
+                            <div className="w-14 h-14 bg-red-600 rounded-full"></div>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="text-center">
+                        <div className="my-4">
+                          {teamsData.map((team) => (
+                            <>
+                              {team.id === game.awayTeamId ? (
+                                <p key={team.id}>{team.name}</p>
+                              ) : undefined}
+                            </>
+                          ))}
+                        </div>
+                        {teamsData.map((team) => (
+                          <>
+                            {team.players.map((player: PlayerInterface) => (
+                              <>
+                                {player.teamId === game.awayTeamId ? (
+                                  <p key={player.id}>
+                                    {player.firstname} {player.lastname} -{" "}
+                                    {player.position}
+                                  </p>
+                                ) : undefined}
+                              </>
+                            ))}
+                          </>
+                        ))}
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
-            </div>
+            </>
           ))}
       </div>
     </>
